b063040d7b03fe2f004e7f6ab76b88afa32b0c39
jihoon0065 <jihoon0065@gmail.com>
2026-04-08 09:58:15 +0000

    week 1 exercise

--- week1/index.js
+++ week1/index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/food",(req,res) => {
+    res.send("Hello world!");
+});
+
+app.listen(8080, () => {
+    console.log("Server listening on Port 8080");
+});
